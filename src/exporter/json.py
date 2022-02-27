b445f0e859d0238f59163fcb17b9e0c29d54c575
Alfan Nur Fauzan <alpancs@gmail.com>
2022-02-27 19:31:21 +0700

    handle recursion limit

--- src/exporter/json.py
+++ src/exporter/json.py
@@ -1,9 +1,12 @@
 import json
+import sys
 
 from harf import Harf
 
 
 def export(harf: Harf, dst_path: str) -> None:
+    default_recursion_limit = sys.getrecursionlimit()
+    sys.setrecursionlimit(10111)
     with open(dst_path, 'w') as dst_file:
         json.dump(
             obj=harf,
@@ -13,3 +16,4 @@ def export(harf: Harf, dst_path: str) -> None:
             separators=(',', ':'),
             default=vars,
         )
+    sys.setrecursionlimit(default_recursion_limit)
