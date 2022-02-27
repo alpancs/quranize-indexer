ccc9eea770f4e621d8e9804f1bb034610c2c8e10
Alfan Nur Fauzan <alpancs@gmail.com>
2022-02-27 11:41:03 +0700

    make rust output more compact

--- src/exporter/rust.py
+++ src/exporter/rust.py
@@ -13,13 +13,18 @@ def export(harf: Harf, dst_path: str) -> None:
 class RustBuilder:
     def __init__(self, dst: typing.TextIO) -> None:
         self.dst = dst
-        self.dst.write('struct Harf {content: char, next_harfs: Vec<Harf>, locations: Vec<u16>}\n\n')
-        self.dst.write('let quran_index = ')
+        self.dst.write(r'struct Harf {content: char, next_harfs: Vec<Harf>, locations: Vec<u16>}')
+        self.dst.write('\n\nlet quran_index = ')
 
     def build(self, harf: Harf) -> None:
-        self.dst.write(f"Harf {{'{harf.content}', vec![")
+        self.dst.write("Harf{'%s',vec![" % harf.content)
         for i, next_harf in enumerate(harf.next_harfs):
             if i > 0:
-                self.dst.write(', ')
+                self.dst.write(',')
             self.build(next_harf)
-        self.dst.write(f"], vec!{harf.locations}}}")
+        self.dst.write('],vec![')
+        for i, location in enumerate(harf.locations):
+            if i > 0:
+                self.dst.write(',')
+            self.dst.write(str(location))
+        self.dst.write(']}')
