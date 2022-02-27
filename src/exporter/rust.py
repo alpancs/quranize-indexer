f908ab96b06da4297dbf382ecfcf7e3d5c229635
Alfan Nur Fauzan <alpancs@gmail.com>
2022-02-27 22:54:04 +0700

    refactor rust output

--- src/exporter/rust.py
+++ src/exporter/rust.py
@@ -7,40 +7,26 @@ def export(harf: Harf, dst_path: str) -> None:
     with open(dst_path, 'w') as dst_file:
         builder = RustBuilder(dst_file)
         builder.build(harf)
-        builder.dst.write('\n}\n')
+        builder.dst.write(' }\n')
 
 
 class RustBuilder:
     def __init__(self, dst: typing.TextIO) -> None:
         self.dst = dst
-        self.dst.write(r'''pub struct Harf {
-    content: char,
-    next_harfs: Vec<Harf>,
-    locations: Vec<u16>,
-}
-
-impl Harf {
-    fn new(content: char, next_harfs: Vec<Harf>, locations: Vec<u16>) -> Self {
-        Self {
-            content,
-            next_harfs,
-            locations,
-        }
-    }
-}
-
-pub fn get_quran_index() -> Harf {
-    ''')
+        self.dst.write(
+            r'pub struct Harf {content: char, next_harfs: Vec<Harf>, locations: Vec<u16>}'
+            '\n\npub fn get_quran_index() -> Harf { '
+        )
 
     def build(self, harf: Harf) -> None:
-        self.dst.write("Harf::new('%s',vec![" % harf.content)
+        self.dst.write("Harf{content:'%s',next_harfs:vec![" % harf.content)
         for i, next_harf in enumerate(harf.next_harfs):
             if i > 0:
                 self.dst.write(',')
             self.build(next_harf)
-        self.dst.write('],vec![')
+        self.dst.write('],locations:vec![')
         for i, location in enumerate(harf.locations):
             if i > 0:
                 self.dst.write(',')
             self.dst.write(str(location))
-        self.dst.write('])')
+        self.dst.write(']}')
